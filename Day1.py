d27788162d7ee3e9f3af9b08cbd883af1514d349
Saraswoti khadka <saraswotikhadka2k2@gmail.com>
2024-05-11 16:15:41 +0545

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,33 @@
+# print("hello world\nhello world\nhi sara")
+# print("hello"+" "+"    sara")
+
+# print('e.g. print("hello"+"world")') 
+# print('String concatenation is done with the "+" sign')
+# input("what is your name?\n")
+# print("Namaste saraswoti"+" "+ input())
+# print(2+3)
+
+# s = input("write your name?")
+# print(len(s))
+
+# name ="sara"
+# name = 12
+# print(name)
+
+# a = 10
+# b = 13 
+# print(a,b)
+# temp = a 
+# a = b 
+# b = temp
+# print(a,b)
+
+#exercise-band name generator
+print("Welcome to the Band Name Generator")
+a = input("What's name of the city you grew up in?\n")
+b = input("What's your pet's name?\n")
+
+print("your band name could be: "+a+" "+b)
+
+
+
