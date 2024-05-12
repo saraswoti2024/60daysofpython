44d1060c1a70990ca1e3d4963aedd4e8a0fab77e
saraswoti <saraswotikhadka2k2@gmail.com>
2024-05-12 18:46:20 +0545

    day2 added

---
+++ Day2.py
@@ -0,0 +1,62 @@
+# print("home"[2])
+# print(1_200+1)
+# num = len(input("name?"))
+# new = str(num)
+# print("your name has " + new + " character")
+# print(type(num))
+# a = 12345
+# b = float(a) #changing datatype
+# print(type(b+12.345))
+
+#exercise 
+# anum = input("give two digit number: ")
+# print(int(anum[0])+int(anum[1]))
+
+# print(2*3) #2 multiply 3
+# print(2**3) #2 power 3
+# print(2+3)
+# #PEMDAS RULE((),**,*/,+-)
+# print(3*3+3/3-3) #7 
+
+#exercise
+# weight = float(input("Enter your weight?"))
+# height = float(input("Enter your height?"))
+# BMI = weight/(height**2)
+# print("your BMI is : ", round(BMI,3))        #round .2f jasto 
+
+# print(10 // 3)     #int ma answer dinxa ani convert int ma hunxa
+# print(12.34++12.345)
+
+# score = 0
+# height = 12
+# isw = 15.67
+# print(f"your score is {score},your height is {height}\n your isw is {isw}")     #prints in same line without type conversion
+
+#exercise-how many days,week,month left to live
+# age = int(input("what is your age?"))
+# yrs = 90 - age
+# x = yrs*365
+# y = yrs*52
+# z = yrs*12
+# print(f"you have {x} days left,you have {y} months left, you have {z} week left ")
+
+#exercise- tip calculator
+
+# amount = float(input("what was the total bill: "))
+# tip = float(input("What percentage tip would you like to give?10,12 or 15?"))
+# people = int(input("How many people to split the bill?"))
+# total = (tip/100) * amount + amount
+# split = total/people
+# print(f"Each person should pay ${split}")
+# print(("%.3f" % split)) #3 decimal samma dekhauxa . paxi
+# print("{:.3f}".format(split)) #aarko method same as mathi
+
+
+
+
+
+
+
+
+
+
