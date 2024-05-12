f42c92658fbd166e2b60adef50971157afac7841
saraswoti <saraswotikhadka2k2@gmail.com>
2024-05-12 18:53:46 +0545

    print,input

--- Day1.py
+++ Day1.py
@@ -23,11 +23,11 @@
 # print(a,b)
 
 #exercise-band name generator
-print("Welcome to the Band Name Generator")
-a = input("What's name of the city you grew up in?\n")
-b = input("What's your pet's name?\n")
+# print("Welcome to the Band Name Generator")
+# a = input("What's name of the city you grew up in?\n")
+# b = input("What's your pet's name?\n")
 
-print("your band name could be: "+a+" "+b)
+# print("your band name could be: "+a+" "+b)
 
 
 
