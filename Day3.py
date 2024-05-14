4327474fadf21216ea199f82de689112b4ce055c
saraswoti <saraswotikhadka2k2@gmail.com>
2024-05-14 19:01:12 +0545

    Control Flow and Logical Operators

--- Day3.py
+++ Day3.py
@@ -101,33 +101,33 @@
 #logical operators- and,or,not
 
 #exercise- love calculator
-# print("Welcome to love calculator!")
-# name1 = input("Enter your name: ")
-# name2 = input("Enter your lover's name: ")
-# combined = name1+name2
-# lowercase = combined.lower()
+print("Welcome to love calculator!")
+name1 = input("Enter your name: ")
+name2 = input("Enter your lover's name: ")
+combined = name1+name2
+lowercase = combined.lower()
 
-# t= lowercase.count("t")
-# r= lowercase.count("r")
-# u= lowercase.count("u")
-# e= lowercase.count("e")
+t= lowercase.count("t")
+r= lowercase.count("r")
+u= lowercase.count("u")
+e= lowercase.count("e")
 
-# true = t+r+u+e
+true = t+r+u+e
 
-# l= lowercase.count("l")
-# o= lowercase.count("o")
-# v= lowercase.count("v")
-# e= lowercase.count("e")
+l= lowercase.count("l")
+o= lowercase.count("o")
+v= lowercase.count("v")
+e= lowercase.count("e")
 
-# love = l+o+v+e
+love = l+o+v+e
 
-# score = str(true+love)
-# print(f"your total love percentage is: {score}%")
-# score = int(score)
-# if (score<10) or (score>90):
-#     print("you go together like coke and mentos")
-# elif (score>=40) and (score<=50):
-#     print("you are alright together")
+score = str(true+love)
+print(f"your total love percentage is: {score}%")
+score = int(score)
+if (score<10) or (score>90):
+    print("you go together like coke and mentos")
+elif (score>=40) and (score<=50):
+    print("you are alright together")
 
 #exercise-treasure island
 
@@ -155,25 +155,25 @@
 #      print("won!")
 
 #practice-saraswoti's horror house game
-print("Welcome to saraswoti's horror house")
+# print("Welcome to saraswoti's horror house")
 
-choice1 = input("choose which house you want to go\nspider,dance,suprise: ")
+# choice1 = input("choose which house you want to go\nspider,dance,suprise: ")
 
-if(choice1=="spider"):
- choice2 = input("wow!nice choice.you're so close to winning!!\nthere are three doors, choose one blue,red,black: ")
- if(choice2=="blue" or choice2=="red"):
-  print("muahahahaha, you're out heehehehe")
- elif choice2=="black":
-  print("CONGRAGULATIONS, YOU WON!!!")
+# if(choice1=="spider"):
+#  choice2 = input("wow!nice choice.you're so close to winning!!\nthere are three doors, choose one blue,red,black: ")
+#  if(choice2=="blue" or choice2=="red"):
+#   print("muahahahaha, you're out heehehehe")
+#  elif choice2=="black":
+#   print("CONGRAGULATIONS, YOU WON!!!")
 
-elif choice1 == "dance":
- choice2 = input("you like dancing huh?!, i will show you how to dance.paparapapaprapap\nchose one dance hiphop,classical,belly: " )
- if(choice2=="hiphop"):
-  print("go outside doing hiphop .youre out!")
- elif(choice2=="classical"):
-  print("you won my horror house,but not the main prize dumbass .try again!")
- else:
-  print("YOU WON!!!SHAKE YOUR HIP NOW HAHAHA")
+# elif choice1 == "dance":
+#  choice2 = input("you like dancing huh?!, i will show you how to dance.paparapapaprapap\nchose one dance hiphop,classical,belly: " )
+#  if(choice2=="hiphop"):
+#   print("go outside doing hiphop .youre out!")
+#  elif(choice2=="classical"):
+#   print("you won my horror house,but not the main prize dumbass .try again!")
+#  else:
+#   print("YOU WON!!!SHAKE YOUR HIP NOW HAHAHA")
 
-elif choice1=="suprise":
- print("you're eliminated,you idiot!!")
+# elif choice1=="suprise":
+#  print("you're eliminated,you idiot!!")
