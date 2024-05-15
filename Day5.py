20be0bdd1c86111312550523ac6a7b34f8b0f947
saraswoti <saraswotikhadka2k2@gmail.com>
2024-05-15 21:09:38 +0545

    python loops

---
+++ Day5.py
@@ -0,0 +1,39 @@
+##FOR LOOP USING LIST
+# fruits = ["apple","peach","pear"]
+
+# for fruit in fruits:
+#  print(fruit)
+#  print(fruit+"pie")
+ 
+#exercise-1 average height
+# student_height = [180,124,144,145,160,200]
+# total =0
+# i=0
+# for height in student_height:
+#     total +=height
+#     i+=1
+
+# average = total/i
+# print(f"the total average height is: {round(average,3)}")
+
+#exercise-2 highest score
+
+# student_score = [78,22,90,25,16,23]
+# max = 0
+# min = student_score[0]
+# for score in student_score:  #to compare starting with index 1 student_score[1:]
+#  if(score>max):
+#   max = score
+ 
+#  if(score<min):
+#   min = score
+
+# print(f"your maximum score : {max}")
+# print(f"your minimum score: {min}")
+      
+
+
+
+
+
+
