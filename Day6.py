e7270ace2f3b1e5692975d2258bb51bdd436b9bc
saraswoti <saraswotikhadka2k2@gmail.com>
2024-05-18 23:49:12 +0545

    Update Day6.py

--- Day6.py
+++ Day6.py
@@ -35,3 +35,19 @@ while(i<15):
 #         step()
 #     else:
 #         move()
+
+
+##maze
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
