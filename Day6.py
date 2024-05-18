5a171d4cc8029b69d6ccb58997d588032327fd3e
saraswoti <saraswotikhadka2k2@gmail.com>
2024-05-18 22:53:08 +0545

    Python Functions & Karel
    skipped two days was not feeling good, will be consistent again

---
+++ Day6.py
@@ -0,0 +1,37 @@
+
+#function
+# def myfunction():
+#  print("hi sara")
+#  print("you can do it!")
+
+# for i in range(0,6):
+#  myfunction()
+
+#while_loop
+i = 12
+while(i<15):
+    print(i)
+    i+=1
+
+#practing in reeborg's world
+
+#hurdle 3
+# def turn():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def step():
+#     turn_left()
+#     move()
+#     turn()
+#     move()
+#     turn()
+#     move()
+#     turn_left()
+
+# while at_goal()!=True:
+#     if(wall_in_front()==True):
+#         step()
+#     else:
+#         move()
