1396ba24a2fd4d7d9cbed6ee0293d6bf1f41bafc
saraswoti <saraswotikhadka2k2@gmail.com>
2024-05-21 20:00:52 +0545

    HangmanProject_Day7
    took me more than 5hour to complete it,learnt alot of thing and mainly i enjoy it :smiley:

--- Day7.py
+++ Day7.py
@@ -1,43 +1,59 @@
 #project hangman
-
+# from replit import clear
 import random
-chosen_word = ["apple","banana","pineapple"]
-ranword = random.choice(chosen_word).lower()
+import os
+import hangman_word07
+# from hangman_word07 import word_list
+from hangman_art07 import logo,stages
+
+ranword = random.choice(hangman_word07.word_list).lower()
 length = len(ranword)
-print(ranword)
-val = str(length)
-print(length)
+lives = 6
+end = False
+print(logo)
 
-# store =[]
-# for i in range(0,length):
-#     store+=ranword[i]
-# print(store)
-
+print ("Welcome to HarryPotter Hangman game.")
+print ("Guess the character of HarryPotter!")
 display =[]
-# for j in range(len(ranword)):
-#     display += "_"
-# print(display)
 
+for j in range(len(ranword)):
+    display += "_"
+print(display)
+print(f"There are total {len(display)} letter")
 # for letter in store:
 #     if(letter==guess):
 #         print(letter)
 #         i+=1
 #         display[i] = letter
 #         print(display)
-guess = input("guess a word: ").lower()
-end = False
-while not False:
+
+while not end:
  guess = input("guess a word: ").lower()
+#  os.system("cls")
+
+ if guess in display:
+   print(f"you've already guessed {guess}")
+   continue
+ 
  for i in range(length):
-    letter = ranword[i]
-    if letter == guess:
-        display[i] = letter
-        print(display[i])
- 
- 
-print(display)
-if "_" not in display:
-end = True
-print("you win")
-else:
-print("you lost")
+  letter = ranword[i]
+  if letter == guess:
+   display[i] = letter
+   print(display)
+
+ if guess not in ranword:    #not in = (!=)
+    print("you guessed " + guess +" which is incorrect. " "you have " + str(lives-1) + " guess left")
+    print(stages[lives])
+    lives -= 1
+ if lives == 0:
+        end = True
+        print("you lose!")
+        print(f"The correct answer is: {ranword}")
+ if "_" not in display:
+     end = True
+     print("you win")       
+
+
+
+
+
