dd58fe615ccf968891de09b8a51f0b3ab429c3bf
saraswoti2024 <saraswotikhadka2k2@gmail.com>
2024-06-01 20:11:19 +0545

    Update Day8.py

--- Day8.py
+++ Day8.py
@@ -51,7 +51,7 @@ import art_day08
 # prime(n=n)
       
 ##project - caesar cipher
-letter = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+letter = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b']
 
 
 def caeser(plaintext,key,which):
