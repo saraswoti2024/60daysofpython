1c75eade60caf5321d5ad0fcd4818b11461486f8
saraswoti2024 <saraswotikhadka2k2@gmail.com>
2024-06-17 16:42:00 +0545

    changes

--- Day13.py
+++ Day13.py
@@ -70,3 +70,4 @@ def visualize():
 
 
 
+print("saraswoti")
---
+++ game_dataday14.py
@@ -0,0 +1,28 @@
+data = [
+   { 'name' : 'Instagram',
+    'follower_count' : 346,
+    'description' : 'social media platform',
+    'country' : 'USA'
+   },
+     { 'name' : 'neymar',
+    'follower_count' : 116,
+    'description' : 'footballer',
+    'country' : 'brazil'
+   },
+     { 'name' : 'messi',
+    'follower_count' : 500,
+    'description' : 'footballer',
+    'country' : 'Argentina'
+   },
+     { 'name' : 'Selena gomez',
+    'follower_count' : 200,
+    'description' : 'singer',
+    'country' : 'USA'
+   },
+     { 'name' : 'rajesh hamal',
+    'follower_count' : 100,
+    'description' : 'Actor',
+    'country' : 'Nepal'
+   },
+
+]
