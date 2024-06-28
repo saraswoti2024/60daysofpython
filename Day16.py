d6526a1b7c34a5ede2418a09caf22c220f82272e
saraswoti2024 <saraswotikhadka2k2@gmail.com>
2024-06-28 08:42:07 +0545

    day16

---
+++ Day16.py
@@ -0,0 +1,34 @@
+
+#----------------turtle module---------------------------
+import turtle
+
+timmy = turtle.Turtle()
+# print(timmy)
+timmy.shape("turtle")
+timmy.color("blue")
+
+myscreen = turtle.Screen()
+#print(myscreen.addshape)
+# print(myscreen.canvwidth)
+# print(myscreen.cv)
+
+myscreen.exitonclick()
+
+
+##-----------------------class,object,constructor--------------------
+# class car:
+#     def __init__(self,color,tier,window) -> None:
+#         self.color=color
+#         self.tier=tier 
+#         self.window=window
+#         self.value = 1000  #by default sabai object ko value same hunxa
+
+
+# car1 = car("blue",4,False)
+# car2 = car("red",3,True)
+
+# print(f"{car1.color}\n{car1.tier}\n{car1.window}\n\n")
+# print(f"{car2.color}\n{car2.tier}\n{car2.window}")
+
+
+
