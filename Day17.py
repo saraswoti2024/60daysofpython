dbc09c0179da3eab3934b34dc063484aff5e5717
saraswoti2024 <saraswotikhadka2k2@gmail.com>
2024-06-29 22:47:13 +0545

    day17

---
+++ Day17.py
@@ -0,0 +1,24 @@
+class user:
+    pass
+
+
+user_1 = user()
+
+
+
+##----------quiz project---------------
+
+# from data import question_data
+
+# class questions:
+#     print("this is class")
+
+
+# question = questions()
+
+# for quest in question_data:
+#     print(quest)
+
+
+
+
