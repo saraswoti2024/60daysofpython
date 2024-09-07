21993e07407aae5554b148c2ba87626ff881a430
saraswoti2024 <saraswotikhadka2k2@gmail.com>
2024-09-07 21:29:34 +0545

    Update Day17.py

--- Day17.py
+++ Day17.py
@@ -22,16 +22,7 @@ print(user_2.followers) #followers ma user xa user means follow(vitrw)
 print(user_2.following)
 ##----------quiz project---------------
 
-# from data import question_data
 
-# class questions:
-#     print("this is class")
-
-
-# question = questions()
-
-# for quest in question_data:
-#     print(quest)
 
 
 
